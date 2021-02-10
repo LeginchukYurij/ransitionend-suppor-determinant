0ee9c43d03467c72974d655004e1a0d094d8a17b
LeginchukYurij <leginchuk.yurij@gmail.com>
2021-02-10 21:54:16 +0300

    first commit

---
+++ trsend-supported.js
@@ -0,0 +1,6 @@
+function transitionSelect() {
+    var el = document.createElement("div");
+    if (el.style.WebkitTransition) return "webkitTransitionEnd";
+    if (el.style.OTransition) return "oTransitionEnd";
+    return 'transitionend';
+}
